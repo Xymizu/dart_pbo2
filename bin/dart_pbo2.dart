afe9217676993d2a111d85e8462c361c17e93abf
Fahri <arifmuhamadfahri@gmail.com>
2025-10-16 16:47:49 +0700

    Initial commit

---
+++ bin/dart_pbo2.dart
@@ -0,0 +1,5 @@
+import 'package:dart_pbo2/dart_pbo2.dart' as dart_pbo2;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_pbo2.calculate()}!');
+}
---
+++ test/dart_pbo2_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_pbo2/dart_pbo2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
